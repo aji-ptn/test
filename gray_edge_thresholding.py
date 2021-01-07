e7d46e589af3f26a1324eeb26d1b61524ffe9eb7
aji-web <ajipamungkastrinurcahyo@gmail.com>
2021-01-07 19:52:27 +0800

    change some

--- gray_edge_thresholding.py
+++ gray_edge_thresholding.py
@@ -1,6 +1,6 @@
 import cv2
-
-nama_img = "C:\\Users\\Aji Pamungkas\\PycharmProjects\\pythonProject1\\Resources/BP_Africo.jpg"
+import numpy
+nama_img = "C:\\Users\\Aji Pamungkas\\PycharmProjects\\pythonProject1\\Resources/BP_Africo.jpg" #import image
 img = cv2.imread(nama_img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edge = cv2.Canny(img,90,90)
@@ -11,3 +11,6 @@ cv2.imshow("gray",gray)
 cv2.imshow("canny",edge)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+#finish
